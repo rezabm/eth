d87f5c92d25b593ff907a6f90a4ef7b491160bc2
Martin Rezab <martin.rezab@kontron.com>
2016-05-10 16:44:58 +0200

    Initial commit

---
+++ nanopool/autoreboot/autoreboot/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace autoreboot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.labelData = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(12, 9);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(22, 13);
+            this.labelStatus.TabIndex = 0;
+            this.labelStatus.Text = "- - -";
+            // 
+            // labelData
+            // 
+            this.labelData.AutoSize = true;
+            this.labelData.Location = new System.Drawing.Point(12, 36);
+            this.labelData.Name = "labelData";
+            this.labelData.Size = new System.Drawing.Size(22, 13);
+            this.labelData.TabIndex = 1;
+            this.labelData.Text = "- - -";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(72, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Reboot";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "- - -";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 165);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelData);
+            this.Controls.Add(this.labelStatus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Label labelData;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ nanopool/autoreboot/autoreboot/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Globalization;
+using Newtonsoft.Json;
+
+namespace autoreboot
+{
+    public partial class Form1 : Form
+    {
+        int secondsInFailure = 0;
+        string worker = string.Empty;
+        public Form1(string[] args)
+        {
+            if (args.Length == 1)
+                worker = args[0];
+            else
+            {
+                MessageBox.Show("Wrong argument.\nUsage: " + Path.GetFileName(Application.ExecutablePath).ToUpper() + " <worker>");
+                Load += (s, e) => Close();
+
+            }
+            InitializeComponent();
+            this.Text = worker;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+
+            string json = client.DownloadString("http://eth.nanopool.org/api/hashrate/0xc8fd764ad39e69ecb857ba7466a0e0057c611c32/" + worker);
+            NanopoolApiWorker data = JsonConvert.DeserializeObject<NanopoolApiWorker>(json);
+
+            try
+            {
+                bool s = bool.Parse(data.Status);
+                double d = double.Parse(data.Data, NumberStyles.Any, CultureInfo.InvariantCulture);
+                if (!s || (d == 0))
+                    secondsInFailure++;
+                else
+                    secondsInFailure = 0;
+
+                labelStatus.Text = "Status: " + s.ToString();
+                labelData.Text = "Data: " + d.ToString();
+
+                label1.Text = "Error state duration: " + secondsInFailure.ToString() + " seconds";
+
+                if (secondsInFailure > 120)
+                    Restart();
+
+
+            }
+            catch
+            {
+            }
+
+        }
+
+        private void Restart()
+        {
+            System.Diagnostics.Process.Start("cmd.exe",  "/c shutdown /r /f /t 0");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Restart();
+        }
+    }
+    public class NanopoolApiWorker
+    {
+
+        [JsonProperty(PropertyName = "status")]
+        public string Status { get; set; }
+
+        [JsonProperty(PropertyName = "data")]
+        public string Data { get; set; }
+    }
+
+
+    }
